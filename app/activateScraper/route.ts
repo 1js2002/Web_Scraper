e009f2118765df0a84b3215135572923b3f01fd0
1js2002 <dgoud99120@gmail.com>
2023-04-29 00:14:00 +0530

    implementing cloud functions

---
+++ app/activateScraper/route.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest } from "next";
+
+type Body = {
+    search: string;
+}
+
+export async function POST(req: NextApiRequest) {
+    const search  = req.body.search;
+}
+
+export async function GET(req: Request) {
+
+
+    return new Response("Hello dinesh!", {
+        status: 200,
+    })
+}
